99fde75eb0c47284bb0acf0332cf01cd8f150226
smallvq123 <smallvq123@qq.com>
2018-10-12 15:45:36 +0800

    add admin

--- price_comparator/web/admin.py
+++ price_comparator/web/admin.py
@@ -3,4 +3,22 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from models import Goods
+
+def disable_commentstatus(modeladmin, request, queryset):
+    queryset.update(is_enable=False)
+
+def enable_commentstatus(modeladmin, request, queryset):
+    queryset.update(is_enable=True)
+
+class GoodsAdmin(admin.ModelAdmin):
+    list_display = (
+    'id', 'g_url', 'g_from', 'g_name', 'price_lasted', 'price_moniter', 'needs_update', 'stock', 'status',
+    'date_created', 'date_updated', 'category')
+    list_display_links = ('id', 'commentator')
+    list_filter = ('commentator', 'article', 'is_enable')
+    actions = [disable_commentstatus, enable_commentstatus]
+
+
 # Register your models here.
+admin.site.register(Goods, GoodsAdmin)
--- price_comparator/web/models.py
+++ price_comparator/web/models.py
@@ -3,17 +3,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # 商品表
 class Goods(models.Model):
-    id = models.AutoField(primary_key=True) #
-    g_url = models.CharField(max_length=2048) #
-    g_from = models.IntegerField() # 商品类型 1，taobao   TODO enum
-    g_name = models.CharField(max_length=256) # 商品名称
-    price_lasted = models.DecimalField(max_digits=10, decimal_places=2) # 商品最新价格
-    price_moniter = models.DecimalField(max_digits=10, decimal_places=2) # 监控价格 当最新价格不一致时 needs_update变成true
-    needs_update = models.BooleanField() # 是否需要更新
-    stock = models.IntegerField() # 库存剩余
-    status = models.IntegerField() # 商品状态 ： -1，停止更新；1，正常更新；
-    date_created = models.DateTimeField('date created', auto_now_add=True)
-    date_updated = models.DateTimeField('date updated', auto_now=True)
-    category = models.CharField('分类',max_length=256)
+    id = models.AutoField(primary_key=True)  #
+    g_url = models.CharField(max_length=2048, verbose_name=('商品链接'))  #
+    g_from_choices = (
+        (1, "淘宝"),
+        (2, "其他（暂未支持）"),
+    )
+    g_from = models.IntegerField(choices=g_from_choices, default=1, verbose_name=('商品来源'))  #  1，taobao
+    g_name = models.CharField(max_length=256, verbose_name=('名称'), null=True, blank=True, default='')  # 商品名称
+    price_lasted = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=('最新官网价'), null=True, blank=True)  # 商品最新价格
+    price_moniter = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=('监控价格'), null=True, blank=True)  # 监控价格 当最新价格不一致时 needs_update变成true
+    needs_update = models.BooleanField(verbose_name=('是否需要更新'))  # 是否需要更新
+    stock = models.IntegerField('库存', null=True, default=-1)  # 库存剩余
+    status_choices = (
+        (-1, "停止更新"),
+        (1, "正常更新"),
+    )
+    status = models.IntegerField(choices=status_choices, default=1, verbose_name=('监控状态'))  # 商品状态 ： -1，停止更新；1，正常更新；
+    date_created = models.DateTimeField('date created', auto_now_add=True, null=True)
+    date_updated = models.DateTimeField('date updated', auto_now=True, null=True)
+    category = models.CharField('分类', max_length=256, null=True, blank=True)
+
